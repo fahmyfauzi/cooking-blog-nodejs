ffd99d1d0129d5ceb1cab36579115ebcf103c5b9
fahmyfauzi <fahmyfauzii@gmail.com>
2023-12-06 22:03:37 +0700

    setup mongodb

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "colors";
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("connected to mongodb database".bgCyan.white);
+});
